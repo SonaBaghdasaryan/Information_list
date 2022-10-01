aa8b3bee3e1fbf23492da999670ce00be62bc5c9
Sona <sona.bagda91@gmail.com>
2022-10-01 21:00:18 +0300

    Third commit

---
+++ main.py
@@ -0,0 +1,7 @@
+"""
+Создать информационную систему позволяющую работать с сотрудниками некой компании \ студентами вуза \ учениками школы
+"""
+from controller import work_with_information
+
+if __name__ == "__main__":
+    work_with_information()
